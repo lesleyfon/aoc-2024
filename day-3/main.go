6e25ae1e56f53d0a8a3f57866d57895cd57f2dfc
lesley <lesleybanadzem2017@gmail.com>
2024-12-04 21:49:43 -0600

    Completed part 2 of day 3

--- day-3/main.go
+++ day-3/main.go
@@ -36,116 +36,72 @@ func isNumber(digit string) bool {
 	return regex.MatchString(digit)
 }
 
-/*
-*
- 1. Import the file
- 2. split the file at mul
- 3. Iterate through the split list.
-    a. if the starting element in the split char does not begin with brackets `(`
-    - continue
-    b. if it begins with an opening brackets `(`
-    - check if the is a closing bracket in the current substr
-    - if not:
-    - continue
-    else:
-    i. create a stack,
-    - create a temp str, create two bools to check if we are currently in the bracket and set it to false
-    one for open and one for close
-    - loop trough every char in the substr
-    - if current char is not a number or an open or close bracket or a comma`,`
-    -- break from the loop
-    - if the current char is a number and open is false
-    -- break from the loop
-    break
-    if the current char is a comma and open is false
-    break
-    if current char is `(` and open is false
-    set open to true and continue
-    if open is true and close is false and the current char is a number?
-    append it to temp str
-    if the current char is a comma and the open is true and close is false
-    push temp to stack
-    set temp to empty str
-    continue
-    if it is a closing char
-    break
-    multiply both int and add it total
-
-________________________________________________________________________
-
-regexToMatchPattern=	^\(\d{1,3},\d{1,3}\)$
-*/
-func partOne() {
-	filePath := "data-part-1.txt"
-	data := readFileAndReturnData(filePath)
-
-	listOfStr := strings.Split(data, "mul")
-	total := 0
-	N := len(listOfStr)
-
-	for i := 0; i < N; i += 1 {
-		char := listOfStr[i]
-
-		startingChar := char[:1]
-		if startingChar != "(" {
-			continue
-		}
-		if !strings.Contains(char, ")") {
-			continue
-		}
-		stack := []string{}
-		open := false
-		temp := ""
-		digits := "1234567890"
-		for _, r := range char {
-			currChar := string(r)
-			if currChar != "(" && currChar != ")" && currChar != "," && !strings.Contains(digits, currChar) {
-				break
-			}
-			if isNumber(currChar) && !open {
-				break
-			}
-			if currChar == "," && !open {
-				break
-			}
-			if currChar == "(" && open {
-				break
-			}
-			if currChar == "(" {
-				open = true
-				continue
-			}
-			if isNumber(currChar) {
-				temp = temp + currChar
-				continue
-			}
-			if currChar == "," {
-				stack = append(stack, temp)
-				temp = ""
-			}
-			if currChar == ")" {
-				stack = append(stack, temp)
-				break
-			}
-		}
-		if len(stack) == 2 {
-			firstDigit, _ := strconv.Atoi(stack[0])
-			secondDigit, _ := strconv.Atoi(stack[1])
-
-			multi := firstDigit * secondDigit
-			total += multi
-
-		}
-	}
-	fmt.Println(total)
-
+func matchesPattern(str string) bool {
+	regex := regexp.MustCompile(`^mul\(\d{1,3},\d{1,3}\)$`)
+	return regex.MatchString(str)
 }
 
-func partTwo() {}
+func partTwo() {
+	filePath := "data-part-2.txt"
+	conutDont := 0
+	countDo := 0
+	data := readFileAndReturnData(filePath)
+	shouldMultiply := true
+	total := 0
+	i := 0
+	n := len(data)
+	for i < n {
+		if i <= len(data)-4 {
+			do := string(data[i : i+4])
+			if do == "do()" {
+				countDo += 1
+				shouldMultiply = true
+				i += 1
+				continue
+			}
+		}
+		if i <= len(data)-7 {
+			dont := string(data[i : i+7])
+			if dont == "don't()" {
+				conutDont += 1
+				shouldMultiply = false
+				i += 1
+				continue
+			}
+		}
+		if !shouldMultiply {
+			i += 1
+			continue
+		}
+		if i <= len(data)-3 {
+			mul := string(data[i : i+3])
+			if mul == "mul" {
+				closingBracketIdx := strings.Index(string(data[i:]), ")") + i
+				chunk := string(data[i : closingBracketIdx+1])
+				matches := matchesPattern(chunk)
+
+				if matches {
+					chunkSplit := strings.Split(chunk, "mul(")
+					secondSplit := strings.Split(chunkSplit[1], ",")
+					firstInt, _ := strconv.Atoi(secondSplit[0])
+					sliceTo := len(secondSplit[1]) - 1
+
+					secondSplitInt, _ := strconv.Atoi((secondSplit[1][0:sliceTo]))
+
+					mul := firstInt * secondSplitInt
+
+					total += mul
+
+				}
+			}
+		}
+		i += 1
+	}
+	println(total, "expected: ", 94455185)
+}
 
 func main() {
-	partOne()
 	partTwo()
 }
 
-// ^\(\d{1,3},\d{1,3}\)$
+// mul(234,456)
--- day-3/readme-part-1.md
+++ day-3/readme-part-1.md
@@ -18,3 +18,111 @@ Only the four highlighted sections are real mul instructions. Adding up the resu
 
 Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
 
+```go
+
+/*
+*
+ 1. Import the file
+ 2. split the file at mul
+ 3. Iterate through the split list.
+    a. if the starting element in the split char does not begin with brackets `(`
+    - continue
+    b. if it begins with an opening brackets `(`
+    - check if the is a closing bracket in the current substr
+    - if not:
+    - continue
+    else:
+    i. create a stack,
+    - create a temp str, create two bools to check if we are currently in the bracket and set it to false
+    one for open and one for close
+    - loop trough every char in the substr
+    - if current char is not a number or an open or close bracket or a comma`,`
+    -- break from the loop
+    - if the current char is a number and open is false
+    -- break from the loop
+    break
+    if the current char is a comma and open is false
+    break
+    if current char is `(` and open is false
+    set open to true and continue
+    if open is true and close is false and the current char is a number?
+    append it to temp str
+    if the current char is a comma and the open is true and close is false
+    push temp to stack
+    set temp to empty str
+    continue
+    if it is a closing char
+    break
+    multiply both int and add it total
+
+________________________________________________________________________
+
+regexToMatchPattern=	^\(\d{1,3},\d{1,3}\)$
+*/
+func partOne() {
+	filePath := "data-part-1.txt"
+	data := readFileAndReturnData(filePath)
+
+	listOfStr := strings.Split(data, "mul")
+	total := 0
+	N := len(listOfStr)
+
+	for i := 0; i < N; i += 1 {
+		char := listOfStr[i]
+
+		startingChar := char[:1]
+		if startingChar != "(" {
+			continue
+		}
+		if !strings.Contains(char, ")") {
+			continue
+		}
+		stack := []string{}
+		open := false
+		temp := ""
+		digits := "1234567890"
+		for _, r := range char {
+			currChar := string(r)
+			if currChar != "(" && currChar != ")" && currChar != "," && !strings.Contains(digits, currChar) {
+				break
+			}
+			if isNumber(currChar) && !open {
+				break
+			}
+			if currChar == "," && !open {
+				break
+			}
+			if currChar == "(" && open {
+				break
+			}
+			if currChar == "(" {
+				open = true
+				continue
+			}
+			if isNumber(currChar) {
+				temp = temp + currChar
+				continue
+			}
+			if currChar == "," {
+				stack = append(stack, temp)
+				temp = ""
+			}
+			if currChar == ")" {
+				stack = append(stack, temp)
+				break
+			}
+		}
+		if len(stack) == 2 {
+			firstDigit, _ := strconv.Atoi(stack[0])
+			secondDigit, _ := strconv.Atoi(stack[1])
+
+			multi := firstDigit * secondDigit
+			total += multi
+
+		}
+	}
+	fmt.Println(total)
+
+}
+
+```
