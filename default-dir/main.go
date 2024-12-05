ad023e505f4d4439f3cbfbb2256cb9d48a2020bf
lesley <lesleybanadzem2017@gmail.com>
2024-12-04 18:09:50 -0600

    Created a default day dir to be copied and pasted.

---
+++ default-dir/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+// https://www.scaler.com/topics/golang/golang-read-file-line-by-line/
+
+func readFileAndReturnData(filePath string) {
+	fmt.Println(filePath)
+}
+
+func partOne() {
+	filePath := ""
+	readFileAndReturnData(filePath)
+}
+
+func partTwo() {}
+
+func main() {
+	partOne()
+	partTwo()
+}
