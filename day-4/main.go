1236fe5355708536245cba5c0f804b36162b3ae9
lesley <lesleybanadzem2017@gmail.com>
2024-12-04 23:36:59 -0600

    Refactored logic for checking if strings

--- day-4/main.go
+++ day-4/main.go
@@ -33,100 +33,35 @@ func getOccurrence(grid [][]string, row int, col int) int {
 	N := len(grid[0])
 	count := 0
 
-	// NORTH := [][]int{{-1, 0}, {-2, 0}, {-3, 0}}
-	if row >= 3 {
-		M := grid[row-1][col]
-		A := grid[row-2][col]
-		S := grid[row-3][col]
-
-		COMBO := M + A + S
-		if COMBO == "MAS" {
-			count += 1
-		}
+	directions := []struct {
+		name    string
+		vectors [][2]int
+		check   func(row int, col int) bool
+	}{
+		{"NORTH",      [][2]int{{-1, 0}, {-2, 0}, {-3, 0}},         func(r, c int) bool { return r >= 3 }},
+		{"NORTH_EAST", [][2]int{{-1, 1}, {-2, 2}, {-3, 3}},         func(r, c int) bool { return r >= 3 && c < N-3 }},
+		{"EAST",       [][2]int{{0, 1}, {0, 2}, {0, 3}},            func(r, c int) bool { return c < N-3 }},
+		{"SOUTH_EAST", [][2]int{{1, 1}, {2, 2}, {3, 3}},            func(r, c int) bool { return r < M-3 && c < N-3 }},
+		{"SOUTH",      [][2]int{{1, 0}, {2, 0}, {3, 0}},            func(r, c int) bool { return r < M-3 }},
+		{"SOUTH_WEST", [][2]int{{1, -1}, {2, -2}, {3, -3}},         func(r, c int) bool { return r < M-3 && c >= 3 }},
+		{"WEST",       [][2]int{{0, -1}, {0, -2}, {0, -3}},         func(r, c int) bool { return c >= 3 }},
+		{"NORTH_WEST", [][2]int{{-1, -1}, {-2, -2}, {-3, -3}},      func(r, c int) bool { return r >= 3 && c >= 3 }},
 	}
 
-	// NORTH_EAST := [][]int{{-1, 1}, {-2, 2}, {-3, 3}}
-	if row >= 3 && col < N-3 {
-		M := grid[row-1][col+1]
-		A := grid[row-2][col+2]
-		S := grid[row-3][col+3]
-
-		COMBO := M + A + S
-		if COMBO == "MAS" {
-			count += 1
-		}
-	}
-	// EAST := [][]int{{0, 1}, {0, 2}, {0, 3}}
-	if col < N-3 {
-		M := grid[row][col+1]
-		A := grid[row][col+2]
-		S := grid[row][col+3]
-
-		COMBO := M + A + S
-		if COMBO == "MAS" {
-			count += 1
-		}
-	}
-	// SOUTH_EAST := [][]int{{1, 1}, {2, 2}, {3, 3}}
-	if row < M-3 && col < N-3 {
-		M := grid[row+1][col+1]
-		A := grid[row+2][col+2]
-		S := grid[row+3][col+3]
-
-		COMBO := M + A + S
-		if COMBO == "MAS" {
-			count += 1
-		}
-	}
-	// SOUTH := [][]int{{1, 0}, {2, 0}, {3, 0}}
-	if row < M-3 {
-		M := grid[row+1][col]
-		A := grid[row+2][col]
-		S := grid[row+3][col]
-
-		COMBO := M + A + S
-		if COMBO == "MAS" {
-			count += 1
-		}
-	}
-	// SOUTH_WEST := [][]int{{1, -1}, {2, -2}, {3, -3}}
-	if row < M-3 && col >= 3 {
-		M := grid[row+1][col-1]
-		A := grid[row+2][col-2]
-		S := grid[row+3][col-3]
-
-		COMBO := M + A + S
-		if COMBO == "MAS" {
-			count += 1
-		}
-	}
-	// WEST := [][]int{{0, -1}, {0, -2}, {0, -3}}
-	if col >= 3 {
-		M := grid[row][col-1]
-		A := grid[row][col-2]
-		S := grid[row][col-3]
-
-		COMBO := M + A + S
-		if COMBO == "MAS" {
-			count += 1
-		}
-	}
-	// NORTH_WEST := [][]int{{-1, -1}, {-2, -2}, {-3, -3}}
-	if col >= 3 && row >= 3 {
-		M := grid[row-1][col-1]
-		A := grid[row-2][col-2]
-		S := grid[row-3][col-3]
-
-		COMBO := M + A + S
-		if COMBO == "MAS" {
-			count += 1
+	for _, dir := range directions {
+		if dir.check(row, col) {
+			combo := ""
+			for _, v := range dir.vectors {
+				combo += grid[row+v[0]][col+v[1]]
+			}
+			if combo == "MAS" {
+				count++
+			}
 		}
 	}
 	return count
 }
 func partOne() {
-
-	// NORTH_WEST := [][]int{{-1, -1}, {-2, -2}, {-3, -3}}
 
 	filePath := "data-part-1.txt"
 	grid := readFileAndReturnData(filePath)
